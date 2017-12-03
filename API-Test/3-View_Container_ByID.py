c574582ac191237cbc097d67cb27054bbbc3d689
Salaheldin Akl <salaheldin.akl@student.dit.ie>
2017-12-03 21:00:59 +0000

    Added Dockerfile.

--- API-Test/3-View_Container_ByID.py
+++ API-Test/3-View_Container_ByID.py
@@ -3,5 +3,5 @@ import requests
 
 ## GET /containers/<id>
 ## Inspect a specific container
-response = requests.get("http://35.205.226.244:5000/containers/43942653c8e1", json={}).json()
+response = requests.get("http://35.205.226.244:5000/containers/127201ecd52f", json={}).json()
 print(json.dumps(response, indent=4))
--- API-Test/4-View_Container_logs.py
+++ API-Test/4-View_Container_logs.py
@@ -3,5 +3,5 @@ import requests
 
 ## GET /containers/<id>/logs
 ## Dump specific container logs
-response = requests.get("http://35.205.226.244:5000/containers/45f9fa3af89a/logs", json={}).json()
+response = requests.get("http://35.205.226.244:5000/containers/127201ecd52f/logs", json={}).json()
 print(json.dumps(response, indent=4))
